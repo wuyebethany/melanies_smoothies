80462b625f7931b497c6ea9c8713745901a2385c
wuyebethany <65140548+wuyebethany@users.noreply.github.com>
2026-05-26 04:30:46 +0000

    update for the indentation error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,24 +1,19 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
-from snowflake.snowpark.function import when_matched
 
-# Write directly to the app
-og_dataset = session.table("smoothies.public.orders")
-    edited_dataset = session.create_dataframe(editable_df)
-    og_dataset.merge(edited_dataset
-                     , (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'])
-                     , [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-                    )
+cnx = st.connection("snowflake")
+session = cnx.session()
+
 st.title(f":cup_with_straw: Pending Smoothie Orders! :cup_with_straw:")
 st.write(
   """
   **Orders that need to be filled**
   """
 )
-cnx = st.connection("snowflake")
-session = cnx.session()
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED") == False).to_pandas()
+
+og_dataset = session.table("smoothies.public.orders")
+my_dataframe = og_dataset.filter(col("ORDER_FILLED") == False).to_pandas()
 
 if not my_dataframe.empty:
     editable_df = st.data_editor(
