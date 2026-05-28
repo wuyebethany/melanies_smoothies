f665e663d11e72ef4b8e9af57e501becffcd2cea
wuyebethany <65140548+wuyebethany@users.noreply.github.com>
2026-05-28 16:56:53 -0700

    Update streamlit_app.py
    update the file

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,31 +6,43 @@ import requests
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-st.title(":cup_with_straw: Pending Smoothie Orders! :cup_with_straw:")
-st.write("**Orders that need to be filled**")
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+	
+# Write directly to the app
+st.title(f":cup_with_straw: Customize Your Smoothie! :cup_with_straw:")
+st.write(
+  """
+  **Choose the fruits you want in your custom Smoothie**
+  """
+)
 
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED") == False).to_pandas()
+from snowflake.snowpark.functions import col
 
-if not my_dataframe.empty:
-    editable_df = st.data_editor(
-        my_dataframe,
-        column_config={
-            "ORDER_FILLED": st.column_config.CheckboxColumn("Order Filled?")
-        },
-        disabled=["INGREDIENTS", "NAME_ON_ORDER"],
-    )
-    if st.button("Submit"):
-        og_dataset = session.table("smoothies.public.orders")
-        edited_dataset = session.create_dataframe(editable_df)
-        og_dataset.merge(
-            edited_dataset,
-            og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID'],
-            [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-        )
-        st.success("Orders updated!", icon="✅")
-        st.rerun()
-else:
-    st.write("No pending orders.")
+name_on_order = st.text_input("Name on Smoothie:")
+st.write("The name on your Smoothie will be:", name_on_order)
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+# st.dataframe(data=my_dataframe, use_container_width=True)
+ingredients_list = st.multiselect(
+'Choose up to 5 ingredients:'
+, my_dataframe
+)
+if ingredients_list:
+    ingredients_string =''
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
+                        values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
+
+    time_to_insert = st.button('Submit Order')
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
+
 
 # Smoothiefroot nutrition info
 smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
