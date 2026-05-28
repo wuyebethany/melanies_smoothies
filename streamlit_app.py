a37a6afdf808c11498abdc8ccfd836794f3a0dba
wuyebethany <65140548+wuyebethany@users.noreply.github.com>
2026-05-28 16:50:01 -0700

    Update streamlit_app.py
    remove "from snowflake.snowpark.context import get_active_session" and add cnx = st.connection("snowflake")
    session = cnx.session()

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from snowflake.snowpark.functions import col, when_matched  # ← fixed import, plural
-from snowflake.snowpark.context import get_active_session
 import requests
 
-session = get_active_session()  # ← define session first
+# session = get_active_session()  # ← define session first
+cnx = st.connection("snowflake")
+session = cnx.session()
 
 st.title(":cup_with_straw: Pending Smoothie Orders! :cup_with_straw:")
 st.write("**Orders that need to be filled**")
