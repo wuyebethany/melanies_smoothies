77cda74c46f0e92f31045fe4d64d82630c141823
wuyebethany <65140548+wuyebethany@users.noreply.github.com>
2026-05-28 17:15:52 -0700

    Update streamlit_app.py
    remove lines and make the lines more compact

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,8 @@ if ingredients_list:
     ingredients_string =''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
+		smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+		sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients, name_on_order)
                         values ('""" + ingredients_string + """','"""+name_on_order+"""')"""
@@ -44,8 +46,5 @@ if ingredients_list:
         st.success('Your Smoothie is ordered, ' + name_on_order + '!', icon="✅")
 
 # Smoothiefroot nutrition info
-# New section to display smoothiefroot nutrition information
-import requests
-smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
-# st.text(smoothiefroot_response.json())  # ← use .json() to display actual data
-sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
+
+
